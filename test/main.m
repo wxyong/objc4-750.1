f7d526ccebf1d3799aad68ae2b11211d0463eb7c
wuxuyong <wuxuyongdz102@163.com>
2019-03-06 21:47:52 +0800

    init

---
+++ test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by aimee on 2019/3/6.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        [NSObject alloc];
+    }
+    return 0;
+}
